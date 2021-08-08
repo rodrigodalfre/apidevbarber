80fdd8e3a7b7d18839a12ae62e771d0df75b4ef6
Rodrigo D <rodrigodalfre@gmail.com>
2021-08-08 02:19:16 -0300

    dev barber: migrate

---
+++ database/migrations/2021_08_08_050650_create_all_tables.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAllTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar')->default('default.png');
+        });
+
+        Schema::create('userfavorites', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_barber');
+        });
+
+        Schema::create('userappointments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_barber');
+            $table->date('ap_datetime');
+        });
+
+        Schema::create('barbers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('avatar')->default('default.png');
+            $table->float('stars')->default(0);
+        });
+
+        Schema::create('barberphotos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_barber');
+            $table->string('url');
+        });
+
+        Schema::create('barberservices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_barber');
+            $table->string('name');
+            $table->float('price');
+        });
+
+        Schema::create('barberfeedback', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_barber');
+            $table->string('name');
+            $table->float('rate');
+            $table->text('body', 200);
+        });
+
+        Schema::create('barberavailability', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_barber');
+            $table->integer('weekday');
+            $table->text('hours');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('userfavorites');
+        Schema::dropIfExists('userappointments');
+        Schema::dropIfExists('barbers');
+        Schema::dropIfExists('barberphotos');
+        Schema::dropIfExists('barberservices');
+        Schema::dropIfExists('barberfeedback');
+        Schema::dropIfExists('barberavailability');
+    }
+}
