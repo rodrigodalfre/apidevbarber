7002e5191f22a8111f2007f7adf01f9bea9b7e6d
Rodrigo D <rodrigodalfre@gmail.com>
2021-08-09 09:29:15 -0300

    jwt.php + auth.php(Api: hash = false), login, register, logout and refresh

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BarberController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/ping', function (){
-    return ['pong' => true];
-});
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::put('/user', [UserController::class, 'update']);
+Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barber/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointments', [BarberController::class, 'setAppointment']);
+Route::post('/barber/{id}/favorite', [BarberController::class, 'addFavorite']);
